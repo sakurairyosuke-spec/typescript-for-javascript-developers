5a4f0a8f2bf156091142aef637e7505d0c1133da
sakurairyosuke-spec <sakurai_r@itg.hitachi.co.jp>
2021-10-20 17:31:36 +0900

    tuple.ts

---
+++ src/number-string.ts
@@ -0,0 +1,12 @@
+export {};
+
+let year = 1976;
+
+console.log(year);
+
+let age = 0x2b;
+
+console.log(age);
+
+let name = 'Ham';
+console.log(name);
---
+++ src/tuple.ts
@@ -0,0 +1,6 @@
+export {};
+
+let profile: [string, number] = ['Ham', 43];
+
+//順序に制約をつけるtuple型
+// profile = [43, 'Ham'];
