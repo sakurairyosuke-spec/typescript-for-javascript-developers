403cd347b8336ba0266cca53d210419ff77b2641
sakurairyosuke-spec <sakurai_r@itg.hitachi.co.jp>
2021-10-25 17:41:26 +0900

    function.ts

---
+++ src/enum.ts
@@ -0,0 +1,20 @@
+export {};
+
+enum  Months {
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December   
+}
+
+console.log(Months.January);
+console.log(Months.February);
+console.log(Months.December);
